224f5065d59c5042f6f7b24f6db61457b2a53d11
ParaPenguin <harry.fox.ic@icloud.com>
2013-11-24 17:36:01 +0000

    Map has info and teams

--- src/main/java/me/parapenguin/parapgm/map/Map.java
+++ src/main/java/me/parapenguin/parapgm/map/Map.java
@@ -1,9 +1,15 @@
 package me.parapenguin.parapgm.map;
 
+import java.util.List;
+
+import me.parapenguin.parapgm.module.InfoModule;
+import me.parapenguin.parapgm.team.MatchTeam;
+
 import org.bukkit.event.Listener;
 
 public class Map implements Listener {
 	
-	
+	InfoModule info;
+	List<MatchTeam> teams;
 	
 }
